47131d8f42f5d088d4f50e7ed6c5cb64011d82d0
Francisco Fernández <ferrycosv@gmail.com>
2020-06-27 13:52:57 +0200

    App state and redefined components as functions, props for data exchange

--- src/components/title.js
+++ src/components/title.js
@@ -1,14 +1,12 @@
 import React from "react";
 
-class Title extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Weather Finder</h1>
-        <p>Find out temperature, conditions and more...</p>
-      </div>
-    );
-  }
+function Title() {
+  return (
+    <div>
+      <h1>Weather Finder</h1>
+      <p>Find out temperature, conditions and more...</p>
+    </div>
+  );
 }
 
 export default Title;
