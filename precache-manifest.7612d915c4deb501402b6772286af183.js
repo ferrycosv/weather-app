6e00f61d1fab03a27e1d283ae78214b9e83314a8
Francisco Fernández <ferrycosv@gmail.com>
2020-07-03 14:39:00 +0200

    Updates

---
+++ precache-manifest.7612d915c4deb501402b6772286af183.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb161306a0f4e0d9378e59d7fcce1c3b",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "014527f5a9912ddfcec9",
+    "url": "/weather-app/static/css/2.1e1ebea9.chunk.css"
+  },
+  {
+    "revision": "eaef5aa6bbaac24d8725",
+    "url": "/weather-app/static/css/main.9a3eae85.chunk.css"
+  },
+  {
+    "revision": "014527f5a9912ddfcec9",
+    "url": "/weather-app/static/js/2.e714b270.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.e714b270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaef5aa6bbaac24d8725",
+    "url": "/weather-app/static/js/main.8a50c56a.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
